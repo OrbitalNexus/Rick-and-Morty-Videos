17ccfd68fcef9f4261619e86a2adde459b29c385
Josh Paul <timeforce@proton.me>
2024-01-24 00:09:21 -0700

    Got character cards working with API and randomized characters each refresh.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,43 +12,47 @@ let card = `<div class="card" style="width: 18rem;">
 
 function getChar() {
 
-   let getRick = "https://rickandmortyapi.com/api/character";
+  let randrm = Math.floor(Math.random() * 42) + 1;
+
+   let getRick = `https://rickandmortyapi.com/api/character?page=${randrm}`;
 
         fetch(getRick)
         .then(res => res.json())
         .then(result => {
             console.log(result);
+            let smith = result.results;
 
 
-            Array.from(result).forEach(data => {
-                let character = data.name;
-                let image = data.image;
-                let gender = data.gender;
-                let status = data.status;
-                let episode = data.location.name;
-                let origin = data.origin.name;
-
-                let card = `<div class="card" style="width: 18rem;">
-                <img src="${image}" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">${character}</h5>
-                  <p class="card-text">${gender}</p>
-                  <p class="card-text">${status}</p>
-                  <p class="card-text">${origin}</p>
-                  <a href="#" class="btn btn-primary">${episode}</a>
-                </div>
-                </div>`;
-
-                $("#displayCard").append(card);
-
-            });
+           morty(smith)
 
     });
 }
 getChar()
 
-function createCard() {
+function morty(data) {
+  data.forEach(char => {
+    let character = char.name;
+    let image = char.image;
+    let gender = char.gender;
+    let status = char.status;
+    let episode = char.location.name;
+    let origin = char.origin.name;
 
+    let card = `<div class="card" style="width: 18rem;">
+    <img src="${image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${character}</h5>
+      <p class="card-text">${gender}</p>
+      <p class="card-text">${status}</p>
+      <p class="card-text">${origin}</p>
+      <a href="#" class="btn btn-primary">${episode}</a>
+    </div>
+    </div>`;
+
+    $("#displayCard").append(card);
+  
+
+});
 }
 
 
