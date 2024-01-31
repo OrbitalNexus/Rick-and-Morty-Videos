4416569b7477150a4e4db7a774961103e9e85ed9
Josh Paul <timeforce@proton.me>
2024-01-31 13:14:55 -0700

    Added comments and refactored code, finished.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 
+// In future will store this in evn file. 
 let apiKey = 'AIzaSyBVegDpBEua2UPD5tSh0cKUawYVuiZ46sw';
 
-
+// Rick and Morty api call with random page assigned.
 function getChar() {
 
   let randrm = Math.floor(Math.random() * 41) + 1;
@@ -15,7 +16,7 @@ function getChar() {
     });
 }
 
-
+// YouTube API with character name assigned.
 function video(name) {
   let gender = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=Rick%20and%20Morty${name}&key=AIzaSyBVegDpBEua2UPD5tSh0cKUawYVuiZ46sw`;
   return fetch(gender).then(function (res) {
@@ -26,6 +27,8 @@ function video(name) {
 })
 }
 
+// Automatic function to get data for API, then foreach loop for array data, Jquery html to display all character cards.
+// hide class assigned is a character is selected to hide display cards.
 getChar().then(function (data) {
     console.log('data :>> ', data);
     data.results.forEach(char => {
@@ -53,30 +56,32 @@ getChar().then(function (data) {
         singleChar(id);
       });
 
-      function singleChar(id) {
+// Single character display card with API call using ID. Fetch data.
+function singleChar(id) {
 
-        let getRick = `https://rickandmortyapi.com/api/character/${id}`;
+    let getRick = `https://rickandmortyapi.com/api/character/${id}`;
       
-             fetch(getRick)
-             .then(res => res.json())
-             .then(result => {
-                 let smith = result;
+      fetch(getRick)
+      .then(res => res.json())
+      .then(result => {
+      let smith = result;
       
-                console.log("singleChar:>>", smith)
-                morty(smith);
+      console.log("singleChar:>>", smith)
+      morty(smith);
       
-         });
-      }
-    
-    function morty(char) {
-        let character = char.name;
-        let image = char.image;
-        let gender = char.gender;
-        let status = char.status;
-        let origin = char.origin.name;
-        let id = char.id;
+  });
+}
 
-        let card = `<div class="card" id="${id}" style="width: 18rem;">
+// Use API data to create card and append Jquery html.
+function morty(char) {
+    let character = char.name;
+    let image = char.image;
+    let gender = char.gender;
+    let status = char.status;
+    let origin = char.origin.name;
+    let id = char.id;
+
+    let card = `<div class="card" id="${id}" style="width: 18rem;">
         <img src="${image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Name: ${character}</h5>
@@ -85,32 +90,27 @@ getChar().then(function (data) {
           <p class="card-text">Origin: ${origin}</p>
         </div>
         </div>`;
-        $("#singleCard").append(card);
+    $("#singleCard").append(card);
 
-        
-
-        video(char.name).then(function (data) {
-          console.log('data :>> ', data);
-          data.items.forEach(vid => {
+// Call YouTube API function and log data, then foreach loop to append Iframe videos.
+    video(char.name).then(function (data) {
+    console.log('data :>> ', data);
+    data.items.forEach(vid => {
     
-           let ivideo = `<iframe class="ivideo" 
+     let ivideo = `<iframe class="ivideo" 
          width="640" 
          height="480" 
          src="https://www.youtube.com/embed/${vid.id.videoId}" 
          allowfullscreen
-     >
-     </iframe>`
+          >
+        </iframe>`
      $("#videoCard").append(ivideo);
-          });
-         
-        
-
-     });
-
-}});
+      });
+    });
+  }});
 });
 
-
+// Refreshes page with Onclick html when button is pushed to refresh characters.
 function refreshPage(){
 
   window.location.reload();
